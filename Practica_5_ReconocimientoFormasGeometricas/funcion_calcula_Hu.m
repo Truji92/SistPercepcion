8959954be0d7f91c460cafd524164bec73121ca8
Truji92 <alejandro.trujillo.7@gmail.com>
2015-05-25 19:04:48 +0200

    Solucionar conflicto de mayusculas/minusculas!

--- Practica_5_ReconocimientoFormasGeometricas/funcion_calcula_Hu.m
+++ Practica_5_ReconocimientoFormasGeometricas/funcion_calcula_Hu.m
@@ -1,77 +1,79 @@
-function [ Hu ] = funcion_calcula_Hu(Ib)
-%FUNCION_CALCULA_HU Summary of this function goes here
-%   Detailed explanation goes here
+function m = Funcion_Calcula_Hu(F)
 
-[x, y] = size(Ib);
-Ib = double(Ib);
+% Cálculo de momentos Hu. El resultado es un vector columna con los 7
+% momentos de Hu calculados sobre la imagen F.
 
-m00 = 0;
-m10 = 0;
-m01 = 0;
 
-for i = 1:x
-    for j = 1:y
-        m00 = m00 + Ib(i,j);
-        m10 = m10 + i*Ib(i,j);
-        m01 = m01 + j*Ib(i,j);
-    end
+if (ndims(F) ~= 2) | issparse(F) | ~isreal(F) | ~(isnumeric(F) | islogical(F))
+    error(['F must be a 2-D, real, nonsparse, numeric or logical matrix']);
 end
 
-X = m10 / m00;
-Y = m01 / m00;
+F = double(F);
 
-w00 = m00;
-%%w10 = w11 = w12 = w20 = w02 = w30 = w21 = w03 = 0;
+[M, N] = size(F);
+[x, y] = meshgrid(1:N, 1:M);
 
-matW = zeros(4);
+x = x(:);
+y = y(:);
+F = F(:);
 
-w = @(p,q,x,y,I) (x-X)^p * (y-Y)^q *I(x,y);
+m.m00 = sum(F);
 
-for i = 1:x
-    for j = 1:y
-        for n = 0:3
-            for m = 0:3
-                matW(n+1,m+1) = matW(n+1,m+1) + w(n,m,i,j,Ib); 
-            end
-        end      
-    end
+if (m.m00 == 0)
+    m.m00 = eps;
 end
 
+m.m10 = sum(x .* F);
+m.m01 = sum(y .* F);
+m.m11 = sum(x .* y .* F);
+m.m20 = sum(x.^2 .* F);
+m.m02 = sum(y.^2 .* F);
+m.m30 = sum(x.^3 .* F);
+m.m03 = sum(y.^3 .* F);
+m.m12 = sum(x .* y.^2 .* F);
+m.m21 = sum(x.^2 .* y .* F);
 
-Wval = @(x,y) matW(x+1,y+1);
-
-matMu = zeros(4);
-for p = 0:3
-    for q = 0:3
-        Wval(p,q)
-        matMu(p+1,q+1) = Wval(p,q)/w00^(1 + (p+q)/2);
-    end
-end 
 
 
-Mu = @(p,q) matMu(p+1,q+1);
+%DIP equations (11.3-14) through (11.3-16).
+xbar = m.m10 / m.m00;
+ybar = m.m01 / m.m00;
 
-Hu = zeros(7,1);
-Hu(1) = Mu(2,0) + Mu(0,2);
-Hu(2) = (Mu(2,0) - Mu(0,2))^2 + 4*Mu(1,1)^2;
-Hu(3) = (Mu(3,0) - 3*Mu(1,2))^2 + (3*Mu(2,1) - Mu(0,3))^2;
-Hu(4) = (Mu(3,0)-Mu(1,2))^2 + (Mu(2,1)-Mu(0,3))^2;
+e.eta11 = (m.m11 - ybar*m.m10) / m.m00^2;
+e.eta20 = (m.m20 - xbar*m.m10) / m.m00^2;
+e.eta02 = (m.m02 - ybar*m.m01) / m.m00^2;
+e.eta30 = (m.m30 - 3 * xbar * m.m20 + 2 * xbar^2 * m.m10) / m.m00^2.5;
+e.eta03 = (m.m03 - 3 * ybar * m.m02 + 2 * ybar^2 * m.m01) / m.m00^2.5;
+e.eta21 = (m.m21 - 2 * xbar * m.m11 - ybar * m.m20 + 2 * xbar^2 * m.m01) / m.m00^2.5;
+e.eta12 = (m.m12 - 2 * ybar * m.m11 - xbar * m.m02 + 2 * ybar^2 * m.m10) / m.m00^2.5;
 
-ceros = Hu == 0;
-Hu(ceros) = 1*exp(-100);
-Hu = abs(log(abs(Hu)));
 
+
+%DIP equations (11.3-17) throught (11.3-23).
+phi(1) = e.eta20 + e.eta02;
+phi(2) = ((e.eta20 - e.eta02)^2) + 4 * e.eta11^2;
+phi(3) = (e.eta30 - 3 * e.eta12)^2 + (3 * e.eta21 - e.eta03)^2;
+phi(4) = (e.eta30 - e.eta12)^2 + (e.eta21 - e.eta03)^2;
+% phi(4) = (e.eta30 + e.eta12)^2 + (e.eta21 + e.eta03)^2;
+phi(5) = (e.eta30 - 3 * e.eta12) * (e.eta30 + e.eta12) * ( (e.eta30 + e.eta12)^2 -  3 * (e.eta21 + e.eta03)^2 ) + (3 * e.eta21 - e.eta03) * (e.eta21 + e.eta03) * ( 3 * (e.eta30 + e.eta12)^2 -  (e.eta21 + e.eta03)^2 );
+phi(6) = (e.eta20 - e.eta02) * ( (e.eta30 + e.eta12)^2 - (e.eta21 + e.eta03)^2 ) + 4 * e.eta11 * (e.eta30 + e.eta12) * (e.eta21 + e.eta03);
+%phi(7) = ( 3 * e.eta21 - e.eta03) * (e.eta30 + e.eta12) * ( (e.eta30 + e.eta12)^2 - 3 * (e.eta21 + e.eta03)^2) + (3 * e.eta12 - e.eta30) * (e.eta21 + e.eta03) * (3 * (e.eta30 + e.eta12)^2 - (e.eta21 + e.eta03)^2 );
+phi(7) = ( 3 * e.eta21 - e.eta30) * (e.eta30 + e.eta12) * ( (e.eta30 + e.eta12)^2 - 3 * (e.eta21 + e.eta03)^2) +(3 * e.eta21 - e.eta03) * (e.eta21 + e.eta03) * (3 * (e.eta30 + e.eta12)^2 - (e.eta21 + e.eta03)^2 );
+
+
+
+for i=1:7
+    phi(i) = phi(i);
 end
 
+for k=1:7
+                
+   if phi(k)==0
+      phi(k)=1e-100;
+   end
 
+end
+            
+m=abs(log(abs(phi)));
 
-        
-%         w10 = w(1,0,i,j,Ib);
-%         w11 = w(1,1,i,j,Ib);
-%         w12 = w(1,2,i,j,Ib);
-%         w20 = w(2,0,i,j,Ib);
-%         w02 = w(0,2,i,j,Ib);
-%         w30 = w(3,0,i,j,Ib);
-%         w21 = w(2,1,i,j,Ib);
-%         w03 = w(0,3,i,jS,Ib);
-  
+m=m';
